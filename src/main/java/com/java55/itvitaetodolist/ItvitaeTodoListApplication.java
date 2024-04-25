84049e57195a006da9e098085e81c6d3ab100041
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-04-25 14:54:16 +0200

    added spring project files

---
+++ src/main/java/com/java55/itvitaetodolist/ItvitaeTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.java55.itvitaetodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItvitaeTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItvitaeTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java55/itvitaetodolist/ItvitaeTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java55.itvitaetodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItvitaeTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
