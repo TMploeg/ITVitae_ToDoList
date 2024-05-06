fe9089385248070fd53c98ab82057c7a02af135d
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-05-06 14:58:52 +0200

    Add second parameter to useEffect

--- frontend/itvitae-todolist/src/components/todolist/ToDoList.jsx
+++ frontend/itvitae-todolist/src/components/todolist/ToDoList.jsx
@@ -33,7 +33,7 @@ export default function Boodschappenlijstje({}) {
             })
             setTodos(newTodos);
         })
-    })
+    }, []);
 
     return (
         <div className="todo-list">
