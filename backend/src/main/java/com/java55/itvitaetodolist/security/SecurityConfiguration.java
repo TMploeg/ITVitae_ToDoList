4678178ec594d53e22e77d1f0f870b1f60a37268
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-05-06 10:48:29 +0200

    added SecurityFilterChain

--- backend/src/main/java/com/java55/itvitaetodolist/security/SecurityConfiguration.java
+++ backend/src/main/java/com/java55/itvitaetodolist/security/SecurityConfiguration.java
@@ -3,8 +3,11 @@ package com.java55.itvitaetodolist.security;
 import io.jsonwebtoken.Jwts;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.crypto.SecretKey;
 
@@ -18,5 +21,21 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(c -> c
+                .anyRequest().permitAll()
+            )
+            .exceptionHandling(exceptionHandlingConfigurer ->
+                exceptionHandlingConfigurer.authenticationEntryPoint(
+                    new UnauthorizedEntryPoint()
+                )
+            )
+            .build();
     }
 }
