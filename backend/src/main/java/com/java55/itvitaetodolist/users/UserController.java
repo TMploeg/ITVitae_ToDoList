3da18b40b7b72053baa30e387a6d6c47d6d28b4d
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-05-06 12:12:03 +0200

    created login/register endpoints

---
+++ backend/src/main/java/com/java55/itvitaetodolist/users/UserController.java
@@ -0,0 +1,78 @@
+package com.java55.itvitaetodolist.users;
+
+import com.java55.itvitaetodolist.security.AuthDTO;
+import com.java55.itvitaetodolist.security.JwtService;
+import com.java55.itvitaetodolist.security.TokenDTO;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class UserController {
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @PostMapping("login")
+    public ResponseEntity<?> login(@RequestBody AuthDTO authDTO) {
+        if (authDTO.username() == null || authDTO.password() == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(ProblemDetail.forStatusAndDetail(
+                            HttpStatus.BAD_REQUEST,
+                            "username and password are required"
+                    ));
+        }
+
+        if (!userService.userExists(authDTO.username())
+                || !userService.isCorrectUserPassword(authDTO.username(), authDTO.password())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(ProblemDetail.forStatusAndDetail(
+                            HttpStatus.BAD_REQUEST,
+                            "username or password incorrect"
+                    ));
+        }
+
+        return ResponseEntity.ok(
+            new TokenDTO(jwtService.generateTokenForUser(authDTO.username()))
+        );
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<?> register(@RequestBody @NotNull AuthDTO authDTO, UriComponentsBuilder ucb) {
+        if (authDTO.username() == null || authDTO.password() == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(ProblemDetail.forStatusAndDetail(
+                            HttpStatus.BAD_REQUEST,
+                            "username and password are required"
+                    ));
+        }
+
+        if (userService.userExists(authDTO.username())
+                || !userService.isValidPassword(authDTO.password())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(ProblemDetail.forStatusAndDetail(
+                            HttpStatus.BAD_REQUEST,
+                            "username or password is invalid"
+                    ));
+        }
+
+        User user = userService.save(
+            authDTO.username(),
+            authDTO.password()
+        );
+
+        return ResponseEntity.ok().build();
+    }
+}
