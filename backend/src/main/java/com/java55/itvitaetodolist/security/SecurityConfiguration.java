7cabce7baaa817853de0f8d9955bacc58391ddb4
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-05-06 10:24:50 +0200

    added PasswordEncoder

--- backend/src/main/java/com/java55/itvitaetodolist/security/SecurityConfiguration.java
+++ backend/src/main/java/com/java55/itvitaetodolist/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.java55.itvitaetodolist.security;
 import io.jsonwebtoken.Jwts;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.crypto.SecretKey;
 
@@ -11,5 +13,10 @@ public class SecurityConfiguration {
     @Bean
     public SecretKey secretKey() {
         return Jwts.SIG.HS256.key().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
